0d25ea0c23483e9e9bc5dbe3d10e6d8b60c87f47
Philip Lundrigan <philiplundrigan@gmail.com>
2018-04-30 09:11:31 -0600

    Add tap support to get symbol

--- get_symbol.py
+++ get_symbol.py
@@ -10,37 +10,45 @@ def chunks(l, n):
         yield l[i:i + n]
 
 
+def split_num_list(ctx, param, value):
+    if value is None:
+        return None
+    try:
+        return [int(x) for x in value.split(',') if x]
+    except ValueError:
+        raise click.BadParameter('List must only contain numbers')
+
+
 @click.command()
 @click.argument('size', nargs=1, type=click.INT)
-@click.option('-t', '--type', default='c', type=click.Choice(['c', 'yaml']), help='Type of output')
+@click.option('-t', '--type', default='c', type=click.Choice(['c', 'yaml', 'plain']), help='Type of output')
 @click.option('-w', '--width', default=32, type=click.INT, help='Width of the lines produced')
 @click.option('--clipboard/--no-clipboard', default=True)
-@click.option('--state', default=1, type=click.INT)
-def get_symbol(size, width, type, clipboard, state):
-    np.set_printoptions(threshold=1e100)
-
-    format_str = f'{{:#0{size + 2}b}}'
-    state = [int(x) for x in format_str.format(state)[2:]]
-
-    symbol, _ = signal.max_len_seq(size, state=state)
+@click.option('--taps', default=None, callback=split_num_list)
+def get_symbol(size, width, type, clipboard, taps):
+    symbol, _ = signal.max_len_seq(size, taps=taps)
     symbol_size = len(symbol)
-    symbol = str(list(symbol))
+    str_symbol = str(list(symbol))
 
     # Remove brackets
-    symbol = symbol[1:-1]
+    str_symbol = str_symbol[1:-1]
 
     # Break into lines
-    lines = chunks(symbol.split(), width)
+    lines = chunks(str_symbol.split(), width)
 
     # Join it all up with new lines between lines
 
 
     if type == 'c':
         symbol = '\n    '.join([' '.join(line) for line in lines])
-        out = f"uint8_t symbol[{symbol_size}] = {{\n\t{symbol}\n}};"
+        out = f"// taps: {taps}\n"
+        out += f"uint8_t symbol[{symbol_size}] = {{\n\t{symbol}\n}};"
     elif type == 'yaml':
         symbol = '\n         '.join([' '.join(line) for line in lines])
-        out = f"symbol: [{symbol}]"
+        out = f"# taps: {taps}\n"
+        out += f"symbol: [{symbol}]"
+    elif type == 'plain':
+        out = str(list(symbol))
     else:
         out = "Unknown type!"
 
